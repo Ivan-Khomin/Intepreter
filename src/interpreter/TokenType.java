67bfefc4b450507bf2b19c90e3cb4f11038cd392
Ivan <homin151210@gmail.com>
2024-11-28 14:43:06 +0200

    added a enums in TokenType

--- src/interpreter/TokenType.java
+++ src/interpreter/TokenType.java
@@ -1,20 +1,30 @@
 package interpreter;
 
 public enum TokenType {
+    // Identifiers
     ID,
     PRINT,
     LET,
+    CONST,
+    NULL,
 
+    // Symbols
     L_PAREN,
     R_PAREN,
     COLON,
     SEMICOLON,
     ASSIGN,
 
+    // Data types
     STRING,
     INT,
+    DOUBLE,
+    FLOAT,
+    CHAR,
+    BYTE,
     STRING_LITERAL,
     NUMBER_LITERAL,
 
+    // EOF - enf of file
     EOF,
 }
